2b4c491dee50b881d582e519fd6818c0e113c8ef
Ilgar Habibov <ilgar.habibov@gmail.com>
2024-11-01 18:46:42 +0400

    Practice 23

---
+++ app/src/main/java/az/altacademy/androidgroup2/lessons/animation/motion/Motion2Fragment.kt
@@ -0,0 +1,29 @@
+package az.altacademy.androidgroup2.lessons.animation.motion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import az.altacademy.androidgroup2.R
+import az.altacademy.androidgroup2.databinding.FragmentMotion2Binding
+
+class Motion2Fragment : Fragment() {
+
+    private lateinit var binding: FragmentMotion2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentMotion2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
--- app/src/main/java/az/altacademy/androidgroup2/lessons/animation/motion/MotionFragment.kt
+++ app/src/main/java/az/altacademy/androidgroup2/lessons/animation/motion/MotionFragment.kt
@@ -13,7 +13,6 @@ import az.altacademy.androidgroup2.lessons.animation.CoreFragment
 class MotionFragment : Fragment() {
     private lateinit var binding: FragmentMotionBinding
 
-    private var stateId = R.id.endOfMotion
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,44 +26,18 @@ class MotionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.motionLayout.setTransition(R.id.startOfMotion, R.id.endOfMotion)
-        binding.centerText.setOnClickListener {
-            binding.motionLayout.transitionToEnd()
+        with(binding){
+            centerText.setOnClickListener {
+                motionLayout.currentState
+                if (motionLayout.currentState == motionLayout.startState) {
+                    motionLayout.transitionToEnd()
+                } else {
+                    motionLayout.transitionToStart()
+                }
+            }
         }
 
-        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener{
 
-            override fun onTransitionStarted(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int
-            ) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTransitionChange(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int,
-                progress: Float
-            ) {
-
-            }
-
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-
-            }
-
-            override fun onTransitionTrigger(
-                motionLayout: MotionLayout?,
-                triggerId: Int,
-                positive: Boolean,
-                progress: Float
-            ) {
-                stateId = triggerId
-            }
-
-        })
     }
 
 
